ea111012a87e5b220652aab4d033dff755c1e23e
Valentino <valentino.bernardo.c@gmail.com>
2018-05-05 11:55:19 +0200

    - Show items on marker - Removed transparent background for ios devices for the items section

--- js/main.js
+++ js/main.js
@@ -13,7 +13,12 @@ $(document).ready(function() {
         });
     } else {
         document.body.classList.add('body-background');
-        console.log(device.tablet())
+    }
+
+    if (device.ios()) {
+        const itemsClasses = document.querySelector('#items').classList;
+        itemsClasses.remove('transparent');
+        itemsClasses.add('primary');
     }
 
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
@@ -52,10 +57,7 @@ $(document).ready(function() {
 
 
 const baseUrl = "http://localhost:8080";
-const icons = [];
-icons.push(document.querySelector("#umbrella"));
-icons.push(document.querySelector("#battery"));
-icons.push(document.querySelector("#bag"));
+const iconTemplate = '<i class="fas fa-<<>>"></i>';
 
 function initMap() {
     const DUSSELDORF = {lat: 51.231492, lng: 6.782071};
@@ -71,19 +73,17 @@ function initMap() {
             locations.forEach(location => {
                 const position = new google.maps.LatLng(location.lat, location.lng);
                 const marker = new google.maps.Marker({position: position, map: map});
-                const availableIcons = icons.filter(e => location.items.includes(e.id));
-                tuneMarker(marker, map, geocoder, availableIcons);
+                tuneMarker(marker, map, geocoder, location.items);
             })
         })
         .catch(err => console.log(err));
 }
 
-function tuneMarker(marker, map, geocoder, icons) {
+function tuneMarker(marker, map, geocoder, items) {
     const infoWindow = new google.maps.InfoWindow();
-
     geocoder.geocode({'location': marker.position}, (results, status) => {
         if (status === 'OK') {
-            infoWindow.setContent(`${results[0].formatted_address}<br>${composeActiveIcons(icons)}`);
+            infoWindow.setContent(`${results[0].formatted_address}<br>${drawIcons(items)}`);
         }
     });
 
@@ -96,10 +96,9 @@ function tuneMarker(marker, map, geocoder, icons) {
     });
 }
 
-function getActiveIcons(icons) {
-    return icons.filter(e => e.style.color === 'black')
-}
-
-function composeActiveIcons(icons) {
-    return getActiveIcons(icons).map(e => e.outerHTML).join(' ');
+function drawIcons(items) {
+    items = items.map(item => {
+        if (item === 'battery') { return 'battery-empty' } else if (item === 'bag') { return 'shopping-bag '} else { return 'umbrella' }
+    });
+    return items.map(item => iconTemplate.replace('<<>>', item)).join('');
 }
